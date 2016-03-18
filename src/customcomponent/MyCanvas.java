d91a8124df19f3f3e991389aea2a6ddad1e1dea8
zechao <zechao158@gmail.com>
2016-03-18 03:16:36 +0100

    Update MyCanvas.java

--- src/customcomponent/MyCanvas.java
+++ src/customcomponent/MyCanvas.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package customcomponent;
-//Test  modified
+//Test  modified wtf
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
